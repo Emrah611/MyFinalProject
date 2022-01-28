1934d94874d8c8d2ea0fa0c0d1569c9fe37ce7fb
Emrah Seferli <emrahseferli89@gmail.com>
2022-01-28 13:22:04 +0400

    28.01.2022

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -36,6 +36,7 @@ namespace WebAPI.Controllers
         [HttpGet("getbyid")]
         public IActionResult GetById(int id)
         {
+
             var result = _productService.GetById(id);
             if (result.Success)
             {
